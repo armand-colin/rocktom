87665c3d2ef0bbaa19f1bef92255d589692cad63
armand-colin <56818352+armand-colin@users.noreply.github.com>
2025-12-17 11:41:33 +0100

    added import/export

--- src/ui/levelList/LevelList.tsx
+++ src/ui/levelList/LevelList.tsx
@@ -1,6 +1,6 @@
 import { useResource } from "@niloc/ecs-react";
 import { LevelStorage } from "../../resources/LevelStorage";
-import type { Level } from "../../sound/Level";
+import { Level } from "../../sound/Level";
 import { Button } from "../button/Button";
 import { LiveInstrumentView } from "../liveInstrument/LiveInstrumentView";
 import "./LevelList.scss";
@@ -16,13 +16,51 @@ export function LevelList(props: {
     onClone: (level: Level) => void,
     onCreate: () => void
 }) {
-    const { levels } = useResource(LevelStorage)
+    const storage = useResource(LevelStorage)
+    const { levels } = storage
+
+    function onExport(level: Level) {
+        const json = level.serialize()
+        const blob = new Blob([JSON.stringify(json)], { type: "application/json" })
+        const url = URL.createObjectURL(blob)
+
+        const a = document.createElement('a')
+        a.href = url
+        a.download = `${level.name || "level"}.json`
+        a.click()
+        URL.revokeObjectURL(url)
+    }
+
+    function onImport() {
+        const input = document.createElement('input')
+        input.type = 'file'
+        input.accept = '.json,application/json'
+        input.onchange = () => {
+            if (input.files && input.files.length > 0) {
+                const file = input.files[0]
+                const reader = new FileReader()
+                reader.onload = () => {
+                    try {
+                        const json = JSON.parse(reader.result as string)
+                        const level = Level.deserialize(json)
+                        storage.save(level)
+                    } catch (e) {
+                        alert("Failed to import level: " + e)
+                    }
+                }
+
+                reader.readAsText(file)
+            }
+        }
+        input.click()
+    }
 
     return <div className="LevelList">
         <h1>Rocktom</h1>
         <h2>List of available songs</h2>
 
         <Button onClick={props.onCreate}>Create</Button>
+        <Button onClick={onImport}>Import</Button>
         <ul>
             {
                 levels.map((level) => (
@@ -44,6 +82,9 @@ export function LevelList(props: {
                             props.onClone(level)
                         }}>
                             Clone
+                        </Button>
+                        <Button onClick={() => onExport(level)}>
+                            Export
                         </Button>
                     </li>
                 ))
