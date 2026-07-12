9b83dcc82c43d1ffad5acdce170fcb178ebb54c2
armand-colin <56818352+armand-colin@users.noreply.github.com>
2026-07-12 15:29:58 +0200

    fixed instrument color

--- app/src/sound/instrument/Instrument.ts
+++ app/src/sound/instrument/Instrument.ts
@@ -57,7 +57,7 @@ export class Instrument {
 export class Bass extends Instrument {
 
     static E = new String(0, 0 / 3, "E", Note.fromName("E", 1), new Color("#ff1919"), new Color("#ff1b1b"), new Color("#700c0c"))
-    static A = new String(1, 1 / 3, "A", Note.fromName("A", 1), new Color("#fcc513"), new Color("#726914ff"), new Color("#58500c"))
+    static A = new String(1, 1 / 3, "A", Note.fromName("A", 1), new Color("#fcc513"), new Color("#726914"), new Color("#58500c"))
     static D = new String(2, 2 / 3, "D", Note.fromName("D", 2), new Color("#2d2dff"), new Color("#2626ff"), new Color("#090934"))
     static G = new String(3, 3 / 3, "G", Note.fromName("G", 2), new Color("#ff6a13"), new Color("#ff7a0d"), new Color("#47280f"))
 
