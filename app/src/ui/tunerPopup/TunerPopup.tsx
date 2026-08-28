25675e8fd16b3a7d3e3a1f465d49f4dd035cafc2
armand-colin <56818352+armand-colin@users.noreply.github.com>
2026-08-28 10:33:40 +0200

    Update TunerPopup.tsx

--- app/src/ui/tunerPopup/TunerPopup.tsx
+++ app/src/ui/tunerPopup/TunerPopup.tsx
@@ -7,6 +7,7 @@ import { Bass } from "../../sound/instrument/Instrument"
 import { FineNote } from "../../sound/note/Note"
 import { Button } from "../button/Button"
 import "./TunerPopup.scss"
+import { Popup } from "../popup/Popup"
 
 const bass = new Bass()
 
@@ -17,51 +18,51 @@ export function TunerPopup(props: { instrument: LiveInstrument, close: () => voi
     const cents = useMemo(() => {
         if (!targetString || detectedFrequency <= 0)
             return 0
-        
+
         return FineNote.cents(targetString.note.frequency, detectedFrequency)
     }, [targetString, detectedFrequency])
 
     const status = !locked ? "idle" :
         Math.abs(cents) < 5 ? "success" :
-        Math.abs(cents) < 10 ? "warn" :
-            "error"
+            Math.abs(cents) < 10 ? "warn" :
+                "error"
 
     const t = Math.max(Math.min(cents, 20), -20) / 40 + 0.5
 
 
-    return <div className="TunerOverlay">
-        <div className="body">
-            <Button onClick={props.close}>Close</Button>
-
-            <div className="strings">
-                {
-                    bass.strings.map(s => {
-                        return <Button
-                            onClick={() => tuner.targetString = s}
-                            data-active={s === targetString}
-                            style={{
-                                "--color": "#" + s.color.getHexString()
-                            } as CSSProperties}
-                        >
-                            {s.name}
-                        </Button>
-                    })
-                }
-            </div>
-
-            <p data-locked={locked ? "true" : "false"}>{detectedFrequency > 0 ? `${detectedFrequency.toFixed(2)}Hz` : "—"}</p>
-            <p data-locked={locked ? "true" : "false"}>{targetString && detectedFrequency > 0 ? cents : "—"}</p>
-
-            <div
-                className="tuner"
-                data-status={status}
-                data-locked={locked ? "true" : "false"}
-                style={{
-                    "--t": t,
-                } as CSSProperties}
-            >
-                <div className="caret"></div>
-            </div>
+    return <Popup.BaseContainer className="TunerOverlay">
+        <Popup.BaseTitle
+            title="Tuner"
+            close={props.close}
+        />
+        <div className="strings">
+            {
+                bass.strings.map(s => {
+                    return <Button
+                        onClick={() => tuner.targetString = s}
+                        data-active={s === targetString}
+                        style={{
+                            "--color": "#" + s.color.getHexString()
+                        } as CSSProperties}
+                    >
+                        {s.name}
+                    </Button>
+                })
+            }
         </div>
-    </div>
+
+        <p data-locked={locked ? "true" : "false"}>{detectedFrequency > 0 ? `${detectedFrequency.toFixed(2)}Hz` : "—"}</p>
+        <p data-locked={locked ? "true" : "false"}>{targetString && detectedFrequency > 0 ? cents : "—"}</p>
+
+        <div
+            className="tuner"
+            data-status={status}
+            data-locked={locked ? "true" : "false"}
+            style={{
+                "--t": t,
+            } as CSSProperties}
+        >
+            <div className="caret"></div>
+        </div>
+    </Popup.BaseContainer>
 }
