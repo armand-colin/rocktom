c8f3f8f9486ae2b30b256c3eca5c035787669939
armand-colin <56818352+armand-colin@users.noreply.github.com>
2026-09-02 23:45:48 +0200

    added other color for guitar

--- app/src/ui/levelList/LevelListItem.stories.tsx
+++ app/src/ui/levelList/LevelListItem.stories.tsx
@@ -26,7 +26,7 @@ export const Main = () => {
 
     const level: LevelEntity = {
         ...mockLevel,
-        instrumentTypes: ["bass"],
+        instrumentTypes: ["bass", "guitar"],
         name,
         duration,
     };
